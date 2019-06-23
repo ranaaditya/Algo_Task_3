e56aa5bccb203ae855da4f6a7436314815da376e
Aditya Rana <42575044+ranaaditya@users.noreply.github.com>
2019-06-23 13:36:00 +0530

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,80 +1,43 @@
-//spidern task four...
-//created by aditya_rana on 19 june 2019...
+//spider_task_3 part_2.
+//written by rana_aditya.
 
 #include<bits/stdc++.h>
-#define ll long long  
 using namespace std;
-
-stack<ll> sortStack(stack<ll> &input) 
-{ 
-    stack<ll> tmpStack; 
-  
-    while (!input.empty()) 
-    { 
-         
-        ll tmp = input.top(); 
-        input.pop(); 
-  
-        
-        while (!tmpStack.empty() && tmpStack.top() < tmp) 
-        { 
-           
-            input.push(tmpStack.top()); 
-            tmpStack.pop(); 
-        } 
-  
-       
-        tmpStack.push(tmp); 
-    } 
-  
-    return tmpStack; 
-} 
-
-
-
- void get_strength(){
- 		int n,m;
-	cout<<"enter the length of wall : ";
+void max_strength(){
+	int n,m,k;
+	cout<<"enter the length of wall ";
 	cin>>n;
 	cout<<"enter the number of bricks : ";
 	cin>>m;
 	if(m>=n){
-	
-stack<ll> stak;
-for(int i=0;i<m;i++){
-	ll ent;
-	cin>>ent;
-	stak.push(ent);
+		int ent;
+		std::priority_queue<int,vector<int>,greater<int> > pq;         //min priority queue
+	  std::priority_queue<int> pr;                                     // max priority queue
+	  for(int i=0;i<m;i++){
+	  	cin>>ent;
+	  	pr.push(ent);
+	  }
+	  for(int i=0;i<n;i++){
+	  	pq.push(pr.top());
+	  	pr.pop();
+	  }
+	  while(pr.size()!=0){
+	  	k=pr.top()+pq.top();
+	  	pr.pop();
+	  	pq.pop();
+	  	pq.push(k);
+	  }
+	  cout<<pq.top();
+	  
+		
+	}else cout<<"!!! number of bricks required  are less to construct the wall  !!!";
+
+}
+	int main(){
+		max_strength();
+		return 0;
+		
 	}
 	
-	stak=sortStack(stak);
 
-while(stak.size()!=n){
-	ll a,b;
-	a=stak.top();
-	stak.pop();
-	b=stak.top();
-	stak.pop();
-	
-	stak.push(a+b);
-	stak=sortStack(stak);
-	
-	}
-	
-	cout<< "maximum strengt of wall is : " <<stak.top();
-
-
-
-}else cout<<endl<<"not possible  NUMBER OF BRICKS ARE LESS THAN LENGTH OF WALL !!! \n ....TRY AGAIN .... ";
-
-
-	
-}
-int main(){
-	
-get_strength();
-	
-	return 0;
-	
-}
 
